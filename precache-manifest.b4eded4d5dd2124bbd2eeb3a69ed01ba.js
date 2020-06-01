c05af3bb8a573734a8f11e7b36b1f07d8ab6a56e
unknown <snuff12@snu.ac.kr>
2020-06-01 21:06:20 +0900

    Updates

---
+++ precache-manifest.b4eded4d5dd2124bbd2eeb3a69ed01ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ac074b895b2e952b8b5a91b019f75c",
+    "url": "/liwoomap/index.html"
+  },
+  {
+    "revision": "3024b8245a6106972242",
+    "url": "/liwoomap/static/css/main.3a6c4815.chunk.css"
+  },
+  {
+    "revision": "c97c218febb1ae0b4a04",
+    "url": "/liwoomap/static/js/2.e1e05842.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/liwoomap/static/js/2.e1e05842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3024b8245a6106972242",
+    "url": "/liwoomap/static/js/main.7a9b4eb0.chunk.js"
+  },
+  {
+    "revision": "243996105393384e58e7",
+    "url": "/liwoomap/static/js/runtime-main.abffc6d2.js"
+  }
+]);
